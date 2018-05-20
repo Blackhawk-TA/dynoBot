7d7d6626269f01e5dbbe0bf16b16e54b6a59f750
Blackhawk-TA <daniel@tapadventures.com>
2018-05-20 02:16:44 +0200

    Implemented hooks, which allow calling functions by a specific interval

--- src/core/pythonHandler.js
+++ src/core/pythonHandler.js
@@ -2,6 +2,7 @@ const spawn = require("child_process").spawn;
 
 module.exports = {
 	run: function(path, msg, channel) {
+		console.log(path, msg);
 		var pythonProcess = spawn("python",[path, msg]);
 		pythonProcess.stdout.on('data', function (data){
 			channel.send(data.toString());
