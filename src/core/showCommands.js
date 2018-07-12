1ee7efb767df5f0079286b4020f511d1817875c8
Blackhawk-TA <daniel@tapadventures.com>
2018-07-12 20:47:27 +0200

    [CORE] showCommands: Made command output less technical and adjusted regex

--- src/core/showCommands.js
+++ src/core/showCommands.js
@@ -4,6 +4,13 @@ const commands = require(base + "/cfg/commands.json");
 
 module.exports = {
 	run: function(msg) {
-		msg.channel.send("```json\n" + JSON.stringify(commands, null, 4) + "```");
+		var answer = "```List of regex commands:";
+		commands.forEach(function(command) {
+			var pathArray = command.path.split("/");
+			var fileName = pathArray[pathArray.length - 1];
+			var name = fileName.split(".")[0];
+			answer += `\n ${name}: ${command.regex}`;
+		});
+		msg.channel.send(answer + "```");
 	}
 };
