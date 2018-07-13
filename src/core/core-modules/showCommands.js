57faa5cba2e45b40ced2dee057470a5939f0878a
Blackhawk-TA <daniel@tapadventures.com>
2018-07-14 00:48:46 +0200

    [CORE] showCommands: Minor command output adjustments

--- src/core/core-modules/showCommands.js
+++ src/core/core-modules/showCommands.js
@@ -4,12 +4,12 @@ const commands = require(base + "/cfg/commands.json");
 
 module.exports = {
 	run: function(msg) {
-		var answer = "```List of regex commands:";
+		var answer = "List of regex commands:```";
 		commands.forEach(function(command) {
 			var pathArray = command.path.split("/");
 			var fileName = pathArray[pathArray.length - 1];
 			var name = fileName.split(".")[0];
-			answer += `\n ${name}: ${command.regex}`;
+			answer += `\n${name}: ${command.regex}`;
 		});
 		msg.channel.send(answer + "```");
 	}
