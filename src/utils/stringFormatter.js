996d79a5e6c6e4deb2f255d33417aab36bfad2cf
D069765 <daniel.peters01@sap.com>
2018-08-05 17:06:22 +0200

    [UTILS] stringFormatted: Added utils module

---
+++ src/utils/stringFormatter.js
@@ -0,0 +1,21 @@
+/**
+ * Utils module optimized for re-use in custom js modules to simplify string formatting
+ * @type {{arrayToString: function(Array, string): string}}
+ */
+module.exports = {
+	/**
+	 * Converts an array into a string
+	 * @param {array} array The array to be converted
+	 * @param {string} separator The string between each array item
+	 * @return {string} The formatted array as string
+	 */
+	arrayToString: function(array, separator) {
+		var string = "";
+
+		array.forEach(function(item) {
+			string = string.concat(item, separator);
+		});
+
+		return string.trim().slice(0, -1);
+	}
+};
