3e599a6332fed64ba8c00daa998f5baee8efd01b
D069765 <daniel.peters01@sap.com>
2018-08-08 14:18:46 +0200

    [MODULE] Blyat: Fixed bug with the message array

--- src/py-modules/blyat.py
+++ src/py-modules/blyat.py
@@ -3,11 +3,12 @@ import json
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
+msg = sys.argv[1].split(",")            # Array of input parameters
 url = 'https://bly.at/a'                # Set destination URL here
-post_fields = {'url': sys.argv[1]}      # Set POST fields here
+post_fields = {'url': msg[1]}      # Set POST fields here
 
 request = Request(url, urlencode(post_fields).encode())
 jsonS = urlopen(request).read().decode()
 
-print("Shortened url: " + json.loads(jsonS)['url'])  # This will be the msg that the bot sends
+print("Shortened url: " + str(json.loads(jsonS)['url']))  # This will be the msg that the bot sends
 sys.stdout.flush()  # cleanup
