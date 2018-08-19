980f1e390a9d0a8fd16a00d1358a37585de35f42
Blackhawk-TA <daniel@tapadventures.com>
2018-08-19 14:40:37 +0200

    [MODULE] rconServer: Fixed bug where wrong password was shown

--- src/js-modules/rconServer/connectInfo.js
+++ src/js-modules/rconServer/connectInfo.js
@@ -8,14 +8,14 @@ module.exports = {
 		if (serverCfg[serverName] !== undefined) {
 			var address = serverCfg[serverName]["address"];
 			var port = serverCfg[serverName]["port"];
-			var password = serverCfg[serverName]["rcon_password"];
+			var password = serverCfg[serverName]["password"];
 
 
 			var connectLink = `steam://connect/${address}:${port}/${password}`;
 			var connectCmd = `connect ${address}:${port}; password ${password};`;
 
 			msg.channel.send("You can connect to the server using this link: " + connectLink +
-				"\n Alternatively you can using following console command:```" + connectCmd + "```");
+				"\nAlternatively you can using following console command:```" + connectCmd + "```");
 		} else {
 			msg.channel.send(`There is no server called ${serverName}.`);
 		}
