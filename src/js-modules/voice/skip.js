1d06b1296e38c3be60a9ab7feb2d6a57570c73e3
Blackhawk-TA <tkcblackhawk@gmail.com>
2020-05-01 21:46:04 +0200

    [MODULE] Voice: Improve skip functionality

--- src/js-modules/voice/skip.js
+++ src/js-modules/voice/skip.js
@@ -6,9 +6,15 @@ module.exports = {
 		let oVoiceChannel = msg.getAuthor().getVoiceChannel();
 
 		if (oVoiceChannel) {
-			let oConnection = connectionsHandler.getConnection(oVoiceChannel.getId());
-			oConnection.play();
-			msg.getTextChannel().send("Title skipped.");
+			let oConnection = connectionsHandler.getConnection(oVoiceChannel.getId()),
+				aPlaylist = oConnection.getPlaylist();
+
+			if (aPlaylist.length > 0) {
+				msg.getTextChannel().send(`Title skipped. Playing '${aPlaylist[0]}'`);
+				oConnection.play();
+			} else {
+				msg.getTextChannel().send("You cannot skip the last song in the playlist.")
+			}
 		} else {
 			msg.getTextChannel().send("You can only skip a title when we are in the same voice channel.");
 		}
