cfa2b5cdc9c223f27a8727ec1faa069d9074ffef
Blackhawk <daniel@tapadventures.com>
2020-06-05 19:25:43 +0200

    [MODULE] Voice: Play next track on error

--- src/js-modules/voice/utils/VoiceConnection.js
+++ src/js-modules/voice/utils/VoiceConnection.js
@@ -40,7 +40,13 @@ class VoiceConnection {
 	 */
 	_attachErrorEvent() {
 		this._oConnection.onEvent("error", function(err) {
-			console.error(`${new Date().toLocaleString()}: ${err}`);
+			console.error(`${new Date().toLocaleString()}: VoiceConnection Error: ${err}`);
+			this.play(); //Try to play the next song on error
+		}.bind(this));
+
+		this._oConnection.onEvent("failed", function(err) {
+			console.error(`${new Date().toLocaleString()}: VoiceConnection Failed: ${err}`);
+			this.play(); //Try to play the next song on error
 		}.bind(this));
 
 		this._bErrorEventAttached = true;
@@ -106,6 +112,7 @@ class VoiceConnection {
 
 			this._oConnection.play(ytDownload(this._sCurrentTitleUrl, {
 				filter: "audioonly",
+				quality: "highestaudio",
 				highWaterMark: ONE_MEGABYTE
 			}));
 
