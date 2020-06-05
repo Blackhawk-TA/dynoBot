438ee9c729020189993a47983b8c49176d869f77
Blackhawk <daniel@tapadventures.com>
2020-06-05 19:26:38 +0200

    [MODULE] Voice: Add join message + prevent leave/join being triggered twice

--- src/js-modules/voice/join.js
+++ src/js-modules/voice/join.js
@@ -1,4 +1,5 @@
 const base = require("path").resolve(".");
+const configHandler = require(base + "/src/utils/configHandler");
 const connectionsHandler = require(base + "/src/js-modules/voice/utils/connectionsHandler");
 const VoiceConnection = require(base + "/src/js-modules/voice/utils/VoiceConnection");
 
@@ -6,12 +7,23 @@ module.exports = {
 	run: function(msg) {
 		let oVoiceChannel = msg.getAuthor().getVoiceChannel();
 
-		if (oVoiceChannel) {
+		if (!oVoiceChannel) {
+			msg.getTextChannel().send("I can only join you, if you're in a voice channel.");
+		} else if (connectionsHandler.getConnection(oVoiceChannel.getId())) {
+			msg.getTextChannel().send("There is already an active voice connection in this channel.");
+		} else {
 			oVoiceChannel.join().then(connection => {
 				let oVoiceConnection = new VoiceConnection(connection),
-					aPlaylist = oVoiceConnection.getPlaylist();
+					aPlaylist = oVoiceConnection.getPlaylist(),
+					oVoiceConfig = configHandler.readJSON(base + "/cfg/modules/voice.json", msg.getServer().getId()),
+					bJoinMessageEnabled = oVoiceConfig.voice_join_message.enabled,
+					sJoinMessagePath = oVoiceConfig.voice_join_message.path;
 
 				connectionsHandler.registerConnection(oVoiceConnection);
+
+				if (bJoinMessageEnabled) {
+					oVoiceConnection.getApiConnection().play(sJoinMessagePath);
+				}
 
 				if (aPlaylist.length > 0) {
 					oVoiceConnection.play();
@@ -23,8 +35,6 @@ module.exports = {
 				console.error(`${new Date().toLocaleString()}: ${error}`);
 				msg.getTextChannel().send("Sorry, I could not join you.");
 			});
-		} else {
-			msg.getTextChannel().send("I can only join you, if you're in a voice channel.");
 		}
 	}
 };
--- src/js-modules/voice/leave.js
+++ src/js-modules/voice/leave.js
@@ -5,11 +5,13 @@ module.exports = {
 	run: function(msg) {
 		let oVoiceChannel = msg.getAuthor().getVoiceChannel();
 
-		if (oVoiceChannel) {
+		if (!oVoiceChannel) {
+			msg.getTextChannel().send("You cannot order me to leave a channel in which you are not in.");
+		} else if (!connectionsHandler.getConnection(oVoiceChannel.getId())) {
+			msg.getTextChannel().send("I've already left this channel.");
+		} else {
 			connectionsHandler.unregisterConnection(oVoiceChannel.getId());
 			msg.getTextChannel().send("Ok, I've left the channel.");
-		} else {
-			msg.getTextChannel().send("You cannot order me to leave a channel in which you are not in.");
 		}
 	}
 };
