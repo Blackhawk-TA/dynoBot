9cc0d4b89ae2af4f383e335971c519202bfdcde3
Blackhawk <daniel@tapadventures.com>
2020-06-06 22:17:51 +0200

    [MODULE] Rcon: Fix editRconPassword using deprecated getChannel method

--- src/core/core-modules/messageCleaner.js
+++ src/core/core-modules/messageCleaner.js
@@ -1,5 +1,4 @@
 const base = require("path").resolve(".");
-
 const configHandler = require(base + "/src/utils/configHandler");
 
 const twoWeeks = 14; //in days
@@ -10,23 +9,21 @@ module.exports = {
 		let pathConfig = base + "/cfg/config.json";
 		let amount = configHandler.readJSON(pathConfig, msg.getServer().getId(), "message_cleaner", "amount");
 
-		msg.getTextChannel().getMessages(amount)
-			.then(messages => {
-				let msgToDelete = [];
-				let date = new Date();
+		msg.getTextChannel().getMessages(amount).then(messages => {
+			let msgToDelete = [];
+			let date = new Date();
 
-				messages.forEach(message => {
-					let diffDays = Math.round(Math.abs((message.getCreationDate().getTime() - date.getTime()) / (oneDay)));
-					if ((message.isMentioned(client.getUser()) || message.getAuthor().getId() === client.getUser().getId()) && diffDays < twoWeeks && msg.isDeletable()) {
-						msgToDelete.push(message);
-					}
-				});
-
-				msg.getTextChannel().deleteMessages(msgToDelete);
-				msg.getTextChannel().send(`I've deleted ${msgToDelete.length} messages related to requests regarding me.`);
-			})
-			.catch(err => {
-				console.error(`${new Date().toLocaleString()}: ${err}`);
+			messages.forEach(message => {
+				let diffDays = Math.round(Math.abs((message.getCreationDate().getTime() - date.getTime()) / (oneDay)));
+				if ((message.isMentioned(client.getUser()) || message.getAuthor().getId() === client.getUser().getId()) && diffDays < twoWeeks && msg.isDeletable()) {
+					msgToDelete.push(message);
+				}
 			});
+
+			msg.getTextChannel().deleteMessages(msgToDelete);
+			msg.getTextChannel().send(`I've deleted ${msgToDelete.length} messages related to requests regarding me.`);
+		}).catch(err => {
+			console.error(`${new Date().toLocaleString()}: ${err}`);
+		});
 	}
 };
