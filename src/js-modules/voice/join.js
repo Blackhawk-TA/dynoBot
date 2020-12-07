1c9500def89333e6a0b98c974e85a8b7b9395db8
Blackhawk <daniel.peters1@pm.me>
2020-12-07 18:45:31 +0100

    [MODULE] Voice: Remove join command

--- src/js-modules/voice/join.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	/**
-	 * Will be removed in version 1.3.1
-	 * @deprecated
-	 */
-	run: function(msg) {
-		msg.getTextChannel().send("This command is not needed anymore. Just add a title or playlist instead.");
-	}
-};
